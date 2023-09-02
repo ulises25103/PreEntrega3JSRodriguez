98ba069142711ef3f1529fa03a4bce737684cd63
ulises25103 <ulises25103@gmail.com>
2023-09-01 21:08:48 -0300

    usando fetch

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-let autos
-
-fetch('components/autos.json')
-    .then(response => response.json())
-    .then(data => {
-        autos = data
-        cargarElementosDelAlmacenamientoLocal()
-    })
-
 function toggleSlideOver() {
     document.getElementById('slideover-container').classList.toggle('invisible')
     document.getElementById('slideover-bg').classList.toggle('opacity-0')
@@ -21,7 +12,7 @@ botonCarrito.addEventListener('click', toggleSlideOver)
 function closeSlideOver() {
     const slideoverContainer = document.getElementById('slideover-container')
     const slideover = document.getElementById('slideover')
-
+    
     slideoverContainer.classList.add('invisible')
     slideover.classList.add('translate-x-full')
 }
@@ -33,16 +24,29 @@ const btnCerrar = document.getElementById('btnCerrar')
 btnCerrar.addEventListener('click', closeSlideOver)
 
 
-function crearElementoCarrito(ID) {
-    const auto = conseguirInformacionAuto(ID)
+fetch('components/autos.json')
+    .then(response => response.json())
+    .then(data => {
+        const autos = data
+        console.log(autos)
+    })
+
+    
+
+
+function crearElementoCarrito(elemento) {
+    const id = elemento.getAttribute('data-id')
+    const nombre = elemento.getAttribute('data-nombre')
+    const precio = elemento.getAttribute('data-precio')
+    
     const item = document.createElement('div')
     item.innerHTML = `
         <div class="cart-item">
-            <img src="./public/${auto.id}.avif" data-id="${auto.id}" alt="${auto.nombre}">
+            <img src="./public/${id}.avif" data-id="${id}" alt="${nombre}">
             <div class="item-details mb-2 flex flex-col items-center justify-center">
-                <p class="item-name font-semibold text-gray-900">${auto.nombre}</p>
-                <p class="item-price text-gray-900">${auto.precio}</p>
-                <p class="item-id hidden">${auto.id}</p>
+                <p class="item-name font-semibold text-gray-900">${nombre}</p>
+                <p class="item-price text-gray-900">${precio}</p>
+                <p class="item-id hidden">${id}</p>
                 <div class="mb-2 gap-2 m-3 flex items-center justify-center">
                     <button class="btnEliminar px-4 py-2 bg-red-500 text-white text-xs">Eliminar</button>
                     <button class="btnComprar px-4 py-2 bg-blue-500 text-white text-xs">Comprar</button>
@@ -57,22 +61,15 @@ function crearElementoCarrito(ID) {
     return item
 }
 
-function conseguirInformacionAuto(ID) {
-    return autos.find(producto => producto.id === ID);
-}
 
-function conseguirID(elemento) {
-    return elemento.getAttribute('data-id')
-}
 
 function agregarAlCarrito(elemento) {
     const cartItemsContainer = document.querySelector('.cart-items')
     cartItemsContainer.innerHTML = '';
-    const ID = conseguirID(elemento)
-    const item = crearElementoCarrito(ID)
+    const item = crearElementoCarrito(elemento)
     cartItemsContainer.appendChild(item)
-    configurarBotonesComprar()
-    actualizarAlmacenamientoLocal()
+    configurarBotonesComprar() 
+    actualizarAlmacenamientoLocal() 
 }
 
 const agregarCarritoButtons = document.querySelectorAll('.agregar-btn')
@@ -83,13 +80,13 @@ agregarCarritoButtons.forEach(button => {
     })
 })
 
+
 function agregarAlCarritoPw(elemento) {
     const cartItemsContainer = document.querySelector('.cart-items-pw')
-    const ID = conseguirID(elemento)
-    const item = crearElementoCarrito(ID)
+    const item = crearElementoCarrito(elemento)
     cartItemsContainer.appendChild(item)
-    configurarBotonesComprar()
-    actualizarAlmacenamientoLocal()
+    configurarBotonesComprar() 
+    actualizarAlmacenamientoLocal() 
 }
 
 const powerwallButton = document.querySelectorAll('.agregar-btn-powerwall')
@@ -110,16 +107,18 @@ function actualizarAlmacenamientoLocal() {
         const precio = item.querySelector('.item-price').textContent
         cartData.push({ id, nombre, precio })
     })
-    localStorage.setItem('cart', JSON.stringify(cartData))
+    localStorage.setItem('cart', JSON.stringify(cartData))  
 }
+
+cargarElementosDelAlmacenamientoLocal()
 
 
 function eliminarDelCarrito(event) {
-    const cartItem = event.target.closest('.cart-item')
-    if (cartItem) {
-        cartItem.remove()
-        actualizarAlmacenamientoLocal()
-    }
+const cartItem = event.target.closest('.cart-item')
+if (cartItem) {
+    cartItem.remove()
+    actualizarAlmacenamientoLocal()
+} 
 
 }
 const deleteButtons = document.querySelectorAll('.btnEliminar')
@@ -128,21 +127,24 @@ deleteButtons.forEach(button => {
 })
 
 
+
 function configurarBotonesComprar() {
     const buyButtons = document.querySelectorAll('.btnComprar')
     buyButtons.forEach(button => {
         button.addEventListener('click', eliminarDelCarrito)
-        button.addEventListener('click', () => {
+        button.addEventListener('click', () =>{
             Swal.fire({
                 icon: 'success',
-                title: 'Listo!',
-                text: 'Tu compra fue realizada!',
-                timer: 3000,
-                timerProgressBar: true
+                title:'Listo!',
+                text:'Tu compra fue realizada!',
+                timer:3000,
+                timerProgressBar:true
             })
         })
-    })
+        })
 }
+
+
 
 configurarBotonesComprar()
 
@@ -156,25 +158,26 @@ function cargarElementosDelAlmacenamientoLocal() {
             elemento.setAttribute('data-id', item.id)
             elemento.setAttribute('data-nombre', item.nombre)
             elemento.setAttribute('data-precio', item.precio)
-            const ID = conseguirID(elemento)
-            const elementoCarrito = crearElementoCarrito(ID)
+            const elementoCarrito = crearElementoCarrito(elemento)
             cartItemsContainer.appendChild(elementoCarrito)
         })
     }
 }
 
+
 function agregarElementoDesdeAlmacenamiento(elemento) {
     const cartItemsContainer = document.querySelector('.cart-items')
-    const ID = conseguirID(elemento)
-    const item = crearElementoCarrito(ID)
+    const item = crearElementoCarrito(elemento)
     cartItemsContainer.appendChild(item)
-    actualizarAlmacenamientoLocal()
+    actualizarAlmacenamientoLocal() 
 }
+
+
 
 const agregarAlCarritoBtn = document.querySelectorAll('.agregar-btn, .agregar-btn-powerwall')
 
 agregarAlCarritoBtn.forEach(btn => {
-    btn.addEventListener('click', function () {
+    btn.addEventListener('click', function() {
         btn.classList.add('bg-green-500')
         btn.classList.add('border-green-500')
         setTimeout(() => {
